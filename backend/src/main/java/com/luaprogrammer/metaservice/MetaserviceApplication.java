de88c5d35bdb2ae618992cbd93b40eab1cc18a05
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-11-14 12:47:03 -0300

    Project clean

---
+++ backend/src/main/java/com/luaprogrammer/metaservice/MetaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.luaprogrammer.metaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaserviceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luaprogrammer/metaservice/MetaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luaprogrammer.metaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
