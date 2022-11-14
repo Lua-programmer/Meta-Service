a4e5177a8633d6bea5ddc6c9a9fbcb8555456639
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-11-14 13:05:38 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
 
   return (
+    <>
     <h1>Olá mundo</h1>
+    <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/images/icon.png'
+
+import './styles.css'
+
+function NotificationButton(){
+    return (
+
+        <div className="ms-red-btn">
+            <img src={icon} alt="Notificar funcionário" />
+        </div>
+    )
+}
+
+export default NotificationButton
