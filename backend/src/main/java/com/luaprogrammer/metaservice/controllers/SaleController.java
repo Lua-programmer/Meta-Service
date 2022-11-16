71e64d4deed09e9fd2fe99cb49e48f0f557283f1
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-11-16 13:33:13 -0300

    Date select

--- backend/src/main/java/com/luaprogrammer/metaservice/controllers/SaleController.java
+++ backend/src/main/java/com/luaprogrammer/metaservice/controllers/SaleController.java
@@ -3,11 +3,12 @@ package com.luaprogrammer.metaservice.controllers;
 import com.luaprogrammer.metaservice.entities.Sale;
 import com.luaprogrammer.metaservice.service.SaleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/sales")
@@ -16,7 +17,9 @@ public class SaleController {
     private final SaleService service;
 
     @GetMapping
-    public List<Sale> findSales(){
-        return service.findSales();
+    public Page<Sale> findSales(Pageable pageable,
+                                @RequestParam(value = "minDate", defaultValue = " ") String minDate,
+                                @RequestParam(value = "maxDate", defaultValue = " ") String maxDate){
+        return service.findSales(pageable, minDate, maxDate);
     }
 }
--- backend/src/main/java/com/luaprogrammer/metaservice/service/SaleService.java
+++ backend/src/main/java/com/luaprogrammer/metaservice/service/SaleService.java
@@ -3,16 +3,26 @@ package com.luaprogrammer.metaservice.service;
 import com.luaprogrammer.metaservice.entities.Sale;
 import com.luaprogrammer.metaservice.repositories.SaleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 @RequiredArgsConstructor
 public class SaleService {
     private final SaleRepository repository;
 
-    public List<Sale> findSales() {
-        return repository.findAll();
+    public Page<Sale> findSales(Pageable pageable, String minDate, String maxDate) {
+
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        LocalDate min = minDate.equals(" ") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals(" ") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 }
