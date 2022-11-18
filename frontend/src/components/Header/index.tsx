ed1c16e8e9238d83bcdb5c3fa1ba88b6a4ffeba2
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-11-18 12:59:02 -0300

    refatorando header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -6,9 +6,9 @@ function Header() {
         <header>
             <div className="ms-logo-container">
                 <img src={logo} alt="logo escrito meta service na lateral direita e na lateral esquerda tem a imagem de um círculo com alguns símbolos de dados analíticos" />
-                    <p>Desenvolvido por <a title="Meu Github" href="https://github.com/Lua-programmer" target="_blank" rel='noopener noreferer'>Luana Melissa</a> </p>
+                <p>Desenvolvido por <a title="Meu Github" href="https://github.com/Lua-programmer" target="_blank" rel='noopener noreferer'>Luana Melissa</a> </p>
                 <p>Projeto orientado na Semana Spring React by <a href="https://www.instagram.com/devsuperior.ig" title="IG DevSuperior" target="_blank" rel='noopener noreferer'>@devsuperior.ig</a></p>
-        </div>
+            </div>
         </header >
     )
 }
